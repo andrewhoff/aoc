a59ae40e545d307aac370db1538d291f89a293ba
A Hoff <ahoff@Andrews-Macbook-Pro.local>
2017-12-02 21:06:48 -0600

    initial

---
+++ day1/part1/main.go
@@ -0,0 +1,9 @@
+// AOC day 1, part 1
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Solving CAPTCHA....")
+}
---
+++ day1/part2/main.go
@@ -0,0 +1,9 @@
+// AOC day 1, part 2
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Solving CAPTCHA....")
+}
